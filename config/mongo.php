3990a95dc903f668640e1c230460e53830517a7e
Colin Mollenhour <colin@mollenhour.com>
2011-03-17 20:01:24 -0400

    Clarify configuration options in sample config.php. Fixes #5

--- config/mongo.php
+++ config/mongo.php
@@ -1,8 +1,12 @@
 <?php
-
+/*
+ * See http://www.php.net/manual/en/mongo.construct.php for configuration options.
+ */
 return array(
   'default' => array(
     'database'  => 'db',
+    //'server'  => 'mongodb://localhost:27017',
+    'options'   => array(),
     'profiling' => FALSE
   )
 );
